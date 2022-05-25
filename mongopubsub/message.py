7d5edcbd77f55175139c2ef4ed3c27cf8f4045b3
da6003be-s <da6003be-s@student.lu.se>
2022-05-25 13:28:47 +0200

    message.py: add minimal documentation

--- mongopubsub/message.py
+++ mongopubsub/message.py
@@ -3,6 +3,19 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class Message:
+    """
+    A pubsub message.
+
+    Attributes
+    ----------
+    topic: str
+        The topic to publish to.
+    attributes: dict
+        Attributes to be attached to the message.
+    data: object
+        The data to be published.
+
+    """
     topic: str
     attributes: dict
     data: object
